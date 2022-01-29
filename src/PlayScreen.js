5ec26de2648f5e1cd2632b658b70abdd1b8b216a
Eric Navar <ericnavar@ufl.edu>
2022-01-29 14:09:52 -0500

    refine layout of 4 player's cards

--- src/PlayScreen.js
+++ src/PlayScreen.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import styled from 'styled-components'
-import {sampleData} from './sampleData.js';
+import { yourCards } from './sampleData.js';
 import './PlayScreen.css';
 import Logo from './assets/logo.png';
+
+const PlayScreenMain = styled.main`
+  justify-content:center;
+  display: grid;
+  background-color: #222;
+  height: 100vh;
+`
 
 const LastCardContainer = styled.div`
   position: absolute;
   bottom: 50%;
-  left: 50%
+  left: calc(50% - 20px);
 `
 
 const CardStyledComponent = styled.div`
@@ -30,19 +37,19 @@ const HandContainer = styled.div`
   border-radius: 8px;
   position: absolute;
   bottom: 2px;
-  width: 50%;
-  left: 25%;
   height: 140px;
   padding-top: 12px;
   padding-bottom: 12px;
   padding-right: 12px;
   padding-left: 32px;
+  justify-content: center;
+  display: grid;
 `
 
 const TopPlayerHandContainer = styled.div`
-  position: absolute;
-  top: 8px;
-  left: 50%;
+  margin-top: 8px;
+  display:grid;
+  justify-content: center;
 `
 
 const HiddenCard = styled.div`
@@ -52,6 +59,19 @@ const HiddenCard = styled.div`
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center;
+`
+
+const LeftPlayerHandContainer = styled.div`
+  position: absolute;
+  top: calc(50% - 40px);
+  transform: rotate(90deg);
+`
+
+const RightPlayerHandContainer = styled.div`
+  position: absolute;
+  top: calc(50% - 40px);
+  right: 0;
+  transform: rotate(-90deg);
 `
 
 function Card(props) {
@@ -68,26 +88,43 @@ function Card(props) {
 function PlayScreen() {
   const yourUserName = "greg";
   const topPlayerName = "eric"
+  const [topPlayerCardCount, setTopPlayerCardCount] = React.useState(5);
+  const [leftPlayerCardCount, setLeftPlayerCardCount] = React.useState(5);
+  const [rightPlayerCardCount, setRightPlayerCardCount] = React.useState(5);
   //what cards you have in your hand
-  const [hand, setHand] = React.useState(sampleData[yourUserName]);
+  const [hand, setHand] = React.useState(yourCards);
   const [lastColor, setLastColor] = React.useState("red");
   const [lastValue, setLastValue] = React.useState("1");
-  const players = 2;
+  const players = 4;
+
+  const myHandOffset = (hand.length * 70) / 2;
 
   return (
-    <div>
+    <PlayScreenMain>
       <TopPlayerHandContainer>
-        <div>
-          {sampleData[topPlayerName].map((card,index) => <HiddenCard className="card"/>)}
+        <div style={{display:'max-content'}}>
+          {Array.apply(null, { length: topPlayerCardCount }).map(card => <HiddenCard className="card"/>)}
         </div>
       </TopPlayerHandContainer>
+      <LeftPlayerHandContainer>
+        <div style={{display:'max-content'}}>
+          {Array.apply(null, { length: topPlayerCardCount }).map(card => <HiddenCard className="card"/>)}
+        </div>
+      </LeftPlayerHandContainer>
+      <RightPlayerHandContainer>
+        <div style={{display:'max-content'}}>
+          {Array.apply(null, { length: topPlayerCardCount }).map(card => <HiddenCard className="card"/>)}
+        </div>
+      </RightPlayerHandContainer>
       <LastCardContainer>
         <Card color={lastColor} value={lastValue} />
       </LastCardContainer>
-      <HandContainer>
-        {hand.map((card,index) => <Card key={index} color={card.c} value={card.v}/>)}
+      <HandContainer style={{left:`calc(50% - ${myHandOffset}px`}}>
+        <div style={{width:'max-content'}}>
+          {hand.map((card,index) => <Card key={index} color={card.c} value={card.v}/>)}
+        </div>
       </HandContainer>
-    </div>
+    </PlayScreenMain>
   );
 }
 
--- src/sampleData.js
+++ src/sampleData.js
@@ -1,40 +1,20 @@
-const sampleData = {
-  greg: [
-    {
-      c:"red",
-      v:"9"
-    },
-    {
-      c:"blue",
-      v:"1"
-    },
-    {
-      c:"green",
-      v:"2"
-    },
-    {
-      c:"yellow",
-      v:"7"
-    }
-  ],
-  eric: [
-    {
-      c:"red",
-      v:"9"
-    },
-    {
-      c:"blue",
-      v:"1"
-    },
-    {
-      c:"green",
-      v:"2"
-    },
-    {
-      c:"yellow",
-      v:"7"
-    }
-  ]
-}
+const yourCards = [
+  {
+    c:"red",
+    v:"9"
+  },
+  {
+    c:"blue",
+    v:"1"
+  },
+  {
+    c:"green",
+    v:"2"
+  },
+  {
+    c:"yellow",
+    v:"7"
+  }
+]
 
-export { sampleData };
+export { yourCards };
