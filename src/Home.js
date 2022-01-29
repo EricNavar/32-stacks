3e05e3223336b96f91ad3dd766b5eb2ada1cf169
Yonas Bahre <yonasbahre@ufl.edu>
2022-01-29 16:57:22 -0500

    Finished home page

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,46 @@
 import React from 'react';
+import Logo from './assets/logo.png';
+import Background from './assets/background.png';
 
 function Home() {
   return (
-    <div>
-      <h1>32Stacks</h1>
-      <p>This is literally the best card game that there is.</p>
+    <div id="parent">  
+
+      <img id="logo" src={Logo} alt=""/> 
+
+      <p><i>This is literally the best card game that there is.</i></p>
       <div style={{margin:"2.5rem"}}>
-        <div>
-          <label for="name">Create a username:</label>
-          <input type="text" id="name" name="name" required minlength="4" maxlength="8" size="10"/>
-          <button>set</button>
+        
+        {/* Username input */}
+        <div id="child1">
+          <label style={{textSize:"large"}} for="name">Set nickname: </label>
+          <input style={{width:"200px"}} type="text" id="name" name="name" required minlength="1" maxlength="15" size="10"/>
         </div>
-        <p>
-          or
-        </p>
+
+        <p></p>
+
         <div>
-          <button>Join a game</button>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
+            <a href="#">Create a new game!</a>
+        </div>
+
+        <p>
+          <a style={{fontSize:"24px"}} href="https://www.youtube.com/watch?v=a3Z7zEc7AXQ">or</a>
+        </p>
+
+        <div>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
+            <a href="#">Join an existing game!</a>
+          <p></p>
+          <input type="text" id="link" name="link" placeholder="Enter link here" size="10"/>
+        </div>
+
+        <div id="child2">
+          <a style={{fontSize:"24px"}} href="rules">Game Rules!</a>
         </div>
       </div>
+
+
     </div>
   );
 }
